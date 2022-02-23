984326d9bf9a6394f4e5615769d0fb69264cb6ce
Haithem Ben hadj khalifa <haithembenhadjkhalifa@gmail.com>
2022-02-23 11:14:55 +0100

    added name to admins table

---
+++ database/migrations/2022_02_23_101240_add_name_to_admin.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNameToAdmin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            //
+            $table->string('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            //
+            $table->dropColumn('name');
+        });
+    }
+}
